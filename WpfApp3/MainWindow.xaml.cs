a8b47b6c218057cfc8841725d8234c084a91dbc8
LuminitaIvancea <39344180+LuminitaAndreea@users.noreply.github.com>
2020-06-03 11:23:12 +0300

    Add files via upload

---
+++ WpfApp3/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Text.RegularExpressions;
+
+namespace WpfApp3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Button1_Click_1(object sender, RoutedEventArgs e)
+        {
+            Regex reg = new Regex(text_Pattern.Text);
+            bool result = reg.IsMatch(text_Text.Text);
+            lbl_result.Text = result.ToString();
+        }
+
+        private void OpenWindow(object sender, RoutedEventArgs e)
+        {
+            Reference reference = new Reference();
+            this.Visibility = Visibility.Hidden;
+            reference.Show();
+
+        }
+    }
+}
